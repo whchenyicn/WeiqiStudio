ccb886f672a83e711f4be4cbb99f823e0522acfd
whchenyicn <whchenyicn@gmail.com>
2026-07-30 12:34:45 +0800

    Remove obsolete robots host directive

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    host: 'https://weiqi-studio.vercel.app',
     sitemap: 'https://weiqi-studio.vercel.app/sitemap.xml',
   }
 }
